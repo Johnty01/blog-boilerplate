06cc258b3bce9614513216659fb9bdaa403e23e7
Johnty01 <vibhusdf123@gmail.com>
2024-05-04 23:40:58 +0530

    implemented moderation service and appropriate events handling

--- comments/index.js
+++ comments/index.js
@@ -18,11 +18,11 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', async(req, res) => {
     const commentID = randomBytes(4).toString('hex')
 
-    const content  = req.body.content
+    const { content }  = req.body
 
     const comments = commentsByPostID[req.params.id] || []
 
-    comments.push({ id: commentID, content })
+    comments.push({ id: commentID, content, status: 'pending' })
 
     commentsByPostID[req.params.id] = comments
     await axios.post('http://localhost:4005/events', {
@@ -30,7 +30,8 @@ app.post('/posts/:id/comments', async(req, res) => {
         data:{
             id: commentID,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     })
     res.status(201).send(comments)
@@ -40,8 +41,28 @@ app.get('/posts/comments', (req, res) => {
     res.send(commentsByPostID || [])
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type)
+
+    const { type, data } = req.body
+
+    if(type ==='CommentModerated') {
+        const { postId, id, status, content } = data
+        const comments = commentsByPostID[postId]
+        const comment = comments.find( comment => {
+            return comment.id ===id
+        })
+        comment.status = status
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                content,
+                status
+            }
+        })
+    }
     res.send({})//good to go, empty response
 })
 
--- query/index.js
+++ query/index.js
@@ -9,6 +9,7 @@ app.use(cors())
 const posts = {}
 
 app.get('/posts', (req,res) => {
+    console.log(posts)
     res.send(posts)
 })
 
@@ -16,14 +17,25 @@ app.post('/events', (req,res) => {
     const { type, data } = req.body
 
     if (type === 'PostCreated') {
-        const { id, title } = req.body
+        const { id, title } = data
         posts[id] = { id, title, comments : []}
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data
-        posts[postId].comments.push( { id, content })
+        const { id, content, postId, status } = data
+        posts[postId].comments.push( { id, content, status })
     }
+
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id === id
+        })
+        comment.status = status
+        comment.content = content
+    }
+
     res.send({})
 })
 
